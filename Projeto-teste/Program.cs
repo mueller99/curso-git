02a18b780789004117aecb1bc2e7a1449fee8487
André Felipe Mueller <andremueller2399@gmail.com>
2022-02-09 22:57:54 -0300

    Acrescentada a pergunta como voce esta?

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -8,6 +8,7 @@ namespace Projeto_teste
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Good morning");
+            Console.WriteLine("Como você está hoje?");
 
             Console.ReadLine();
         }
