31dd5140007289d2c15641d1f906d4952b7e8465
André Felipe Mueller <andremueller2399@gmail.com>
2022-02-09 23:21:11 -0300

    Removida mensagem e inserida desejando um bom dia

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -7,8 +7,8 @@ namespace Projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Good morning");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia");
 
             Console.ReadLine();
         }
